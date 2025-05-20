85ab51575c1ea0fcf13d4d3202202ff47414b59b
vitor-henrique-f <vitorcamposfaz@gmail.com>
2025-05-19 22:23:08 -0300

    atualização exercício 2 (completo)

--- exerc.2/src/exerc/pkg2/Exerc2.java
+++ exerc.2/src/exerc/pkg2/Exerc2.java
@@ -1,21 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package exerc.pkg2;
 
-/**
- *
- * @author PC GAMER
- */
 public class Exerc2 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println(substituicaoVogais("casa"));
     }
-    
+
+    public static String substituicaoVogais(String x) {
+        char[] resultado = new char[x.length()];
+
+        for (int i = 0; i < x.length(); i++) {
+            char c = x.charAt(i);
+
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
+                    || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
+                resultado[i] = '*';
+            } else {
+                resultado[i] = c;
+            }
+        }
+
+        return new String(resultado);
+    }
 }
