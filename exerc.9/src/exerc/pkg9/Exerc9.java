3bf5ee31683f72d4c88af354438c38d9c33bb4c4
vitor-henrique-f <vitorcamposfaz@gmail.com>
2025-05-19 22:41:10 -0300

    add exercício 9

---
+++ exerc.9/src/exerc/pkg9/Exerc9.java
@@ -0,0 +1,14 @@
+
+package exerc.pkg9;
+
+
+public class Exerc9 {
+
+    
+    public static void main(String[] args) {
+        System.out.println(porcentagem(200, 10));
+    }
+    public static double porcentagem(double total, double porcentagem) {
+        return (total * porcentagem) / 100;
+    }
+}
